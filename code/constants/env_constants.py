42442c7647957309af41d16a3365594b2ac6bcc2
Mai Tiến Thành <thanhmt@aequitas.vn>
2025-07-08 14:14:38 +0700

    [Fix] change env config

--- code/constants/env_constants.py
+++ code/constants/env_constants.py
@@ -5,8 +5,6 @@ load_dotenv()
 
 class EnvCons:
     BASE_URL = os.environ.get("BASE_URL", "")
-    ICB_PUBLIC_KEY_PATH = os.environ.get(
-        "ICB_PUBLIC_KEY_PATH", "")
     USER_NAME = os.environ.get("USER_NAME", "")
     PASSWORD = os.environ.get("PASSWORD", "")
     CARD_NUMBER = os.environ.get("CARD_NUMBER", "")
@@ -14,8 +12,4 @@ class EnvCons:
     BUCKET_NAME = os.environ.get("BUCKET_NAME", "")
     DISCORD_WEBHOOK_URL = os.environ.get("DISCORD_WEBHOOK_URL", "")
     GG_CREDS_PATH = os.environ.get("GG_CREDS_PATH", "")
-    if ICB_PUBLIC_KEY_PATH and os.path.exists(ICB_PUBLIC_KEY_PATH):
-        with open(ICB_PUBLIC_KEY_PATH, "r") as f:
-            ICB_PUBLIC_KEY = f.read()
-    else:
-        ICB_PUBLIC_KEY = ""
+    ICB_PUBLIC_KEY = os.environ.get("ICB_PUBLIC_KEY", "")
